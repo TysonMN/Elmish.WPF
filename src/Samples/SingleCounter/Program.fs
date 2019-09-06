6686bcf7de13e568c0ea254c7b7ee15d4cb2d90c
Tyson Williams <tyson.williams@blocherconsulting.com>
2019-09-05 23:00:26 -0500

    behaivor preserving transformation to introduce submodules

--- src/Samples/SingleCounter/Program.fs
+++ src/Samples/SingleCounter/Program.fs
@@ -4,41 +4,46 @@ open System
 open Elmish
 open Elmish.WPF
 
-type Model =
-  { Count: int
-    StepSize: int }
+module MainModule =
 
-let init () =
-  { Count = 0
-    StepSize = 1 }
+  type Model =
+    { Count: int
+      StepSize: int }
+  
+  let init () =
+    { Count = 0
+      StepSize = 1 }
+  
+  type Msg =
+    | Increment
+    | Decrement
+    | SetStepSize of int
+    | Reset
+  
+  let update msg m =
+    match msg with
+    | Increment -> { m with Count = m.Count + m.StepSize }
+    | Decrement -> { m with Count = m.Count - m.StepSize }
+    | SetStepSize x -> { m with StepSize = x }
+    | Reset -> init ()
 
-type Msg =
-  | Increment
-  | Decrement
-  | SetStepSize of int
-  | Reset
 
-let update msg m =
-  match msg with
-  | Increment -> { m with Count = m.Count + m.StepSize }
-  | Decrement -> { m with Count = m.Count - m.StepSize }
-  | SetStepSize x -> { m with StepSize = x }
-  | Reset -> init ()
+module BindingModule =
 
-let bindings () : Binding<Model, Msg> list = [
-  "CounterValue" |> Binding.oneWay (fun m -> m.Count)
-  "Increment" |> Binding.cmd Increment
-  "Decrement" |> Binding.cmd Decrement
-  "StepSize" |> Binding.twoWay(
-    (fun m -> float m.StepSize),
-    int >> SetStepSize)
-  "Reset" |> Binding.cmdIf(Reset, (<>) (init ()))
-]
+  let bindings () : Binding<MainModule.Model, MainModule.Msg> list = [
+    "CounterValue" |> Binding.oneWay (fun m -> m.Count)
+    "Increment" |> Binding.cmd MainModule.Increment
+    "Decrement" |> Binding.cmd MainModule.Decrement
+    "StepSize" |> Binding.twoWay(
+      (fun (m:MainModule.Model) -> float m.StepSize),
+      int >> MainModule.SetStepSize)
+    "Reset" |> Binding.cmdIf(MainModule.Reset, (<>) (MainModule.init ()))
+  ]
 
 
 [<EntryPoint; STAThread>]
 let main argv =
-  Program.mkSimpleWpf init update bindings
+  Program.mkSimpleWpf MainModule.init MainModule.update BindingModule.bindings
   |> Program.withConsoleTrace
   |> Program.runWindowWithConfig
       { ElmConfig.Default with LogConsole = true; Measure = true }
