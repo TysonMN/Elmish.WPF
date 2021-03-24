fa693be2d2ec347152ac55482ff4ff628e731497
Tyson Williams <tyson.d.w@gmail.com>
2021-03-24 16:11:04 -0500

    added UndoReset to SingleCounter

--- src/Samples/SingleCounter.Core/Program.fs
+++ src/Samples/SingleCounter.Core/Program.fs
@@ -4,40 +4,86 @@ open Serilog
 open Serilog.Extensions.Logging
 open Elmish.WPF
 
-type Model =
+type BasicModel =
   { Count: int
     StepSize: int }
 
-type Msg =
+type FullModel =
+  { Current: BasicModel
+    Past: BasicModel option }
+
+type BasicMsg =
   | Increment
   | Decrement
   | SetStepSize of int
-  | Reset
 
-let init =
+type FullMsg =
+  | BasicMsg of BasicMsg
+  | Reset
+  | UndoReset of BasicModel
+
+let getStepSize m = m.StepSize
+
+let basicInit =
   { Count = 0
     StepSize = 1 }
 
-let canReset = (<>) init
+let initFul =
+  { Current = basicInit
+    Past = None }
+    
+let canReset m = m.Current <> basicInit
 
-let update msg m =
+let updateBasic msg m =
   match msg with
   | Increment -> { m with Count = m.Count + m.StepSize }
   | Decrement -> { m with Count = m.Count - m.StepSize }
   | SetStepSize x -> { m with StepSize = x }
-  | Reset -> init
 
-let bindings () : Binding<Model, Msg> list = [
+let updateFull msg m =
+  match msg with
+  | BasicMsg msg -> { m with Current = updateBasic msg m.Current }
+  | Reset ->
+      { m with Current = basicInit
+               Past = Some m.Current }
+  | UndoReset bm ->
+      { m with Current = bm
+               Past = None }
+
+let bindingsBasic () : Binding<BasicModel, BasicMsg> list = [
   "CounterValue" |> Binding.oneWay (fun m -> m.Count)
   "Increment" |> Binding.cmd Increment
   "Decrement" |> Binding.cmd Decrement
-  "StepSize" |> Binding.twoWay(
-    (fun m -> float m.StepSize),
-    int >> SetStepSize)
-  "Reset" |> Binding.cmdIf(Reset, canReset)
+  "StepSize" |> Binding.twoWay(getStepSize >> float, int >> SetStepSize)
 ]
 
-let designVm = ViewModel.designInstance init (bindings ())
+let optionMap (f: 'a -> 'b) (optionA: 'a option) : 'b option =
+  match optionA with
+  | Some a ->
+      let b = f a
+      let optionB = Some b
+      optionB
+  | None -> None
+
+
+let helper m =
+  match m.Past with
+  | Some bm -> UndoReset bm
+  | None -> failwith "Was in the none state :("
+
+let bindingsFull () =
+  let basicBindings =
+    bindingsBasic ()
+    |> Bindings.mapModel (fun m -> m.Current)
+    |> Bindings.mapMsg BasicMsg
+  [ "Reset" |> Binding.cmdIf(Reset, canReset)
+    "UndoReset" |> Binding.cmdIf (fun m -> m.Past |> Option.map UndoReset)
+    //"UndoReset" |> Binding.cmdIf (helper, (fun m -> m.Past |> Option.isSome))
+  ] @ basicBindings
+
+
+
+let designVm = ViewModel.designInstance initFul (bindingsFull ())
 
 let main window =
 
@@ -49,6 +95,6 @@ let main window =
       .WriteTo.Console()
       .CreateLogger()
 
-  WpfProgram.mkSimple (fun () -> init) update bindings
+  WpfProgram.mkSimple (fun () -> initFul) updateFull bindingsFull
   |> WpfProgram.withLogger (new SerilogLoggerFactory(logger))
   |> WpfProgram.startElmishLoop window
